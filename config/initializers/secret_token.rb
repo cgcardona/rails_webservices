8771a8f0bed8cf2b6a762bf7d37472f3d5adc8f4
Carlos Cardona <cgcardona@gmail.com>
2013-04-13 17:07:04 -0700

    rails app and Profile controller/views

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+RailsWebservices::Application.config.secret_token = '17e9ce0e4e7e220db9dfa8133c7d9a86eb09812f0c5c1c5b94107395fb78dc12733cf9601cd8b0510ddc167cbac015838e2730c7f4567ceb6777d5e15c8ff7f5'
